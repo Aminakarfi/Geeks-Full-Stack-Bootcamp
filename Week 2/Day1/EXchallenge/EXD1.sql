0d723ff048498c74a202b1d743a45cb0cd24aa47
Amina Karfi <karfi.amina7@gmail.com>
2025-06-30 15:10:34 +0100

    updated

---
+++ Week 2/Day1/EXchallenge/EXD1.sql
@@ -0,0 +1,7 @@
+SELECT COUNT(*) AS total_actors
+FROM actors;
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES ('Chris', 'Hemsworth', '08/11/1983', 0);
+
+SELECT * FROM actors;
