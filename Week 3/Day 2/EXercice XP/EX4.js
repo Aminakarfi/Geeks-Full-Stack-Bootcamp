e2b9682a908ce6cf09e811e3e5de0be0b1b16235
Amina Karfi <karfi.amina7@gmail.com>
2025-07-09 11:48:21 +0100

    updated again

--- Week 3/Day 2/EXercice XP/EX4.js
+++ Week 3/Day 2/EXercice XP/EX4.js
@@ -15,7 +15,7 @@ function hotelCost() {
 function playRideCost() {
     let destination 
     while (true) {
-        let destination = prompt("Where are you going? Enter a number")
+        let destination = prompt("Where are you going? Enter a number");
         if (typeof(destination) === 'string'){
             switch(destination.toLowerCase()){
                case "paris": return confirm("The cost of the ride is 220$");
@@ -32,8 +32,8 @@ function rentalCarCost() {
     let rentalCar
     while (true){
         rentalCar = prompt("How many days are going to rent the car?")
-        if (rentalCar <= 10) return confirm(`the total is ${rentalCar * 40}$`);
-        else return confirm(`the total is ${(rentalCar * 40 * 50) / 100}$`);
+           if (rentalCar <= 10) return confirm(`the total is ${rentalCar * 40}$`);
+           else return confirm(`the total is ${(rentalCar * 40 * 50) / 100}$`);
 
     }
 }
